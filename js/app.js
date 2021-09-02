64efc228999e5f936a08ba53c1133ca1d361f1d1
Muaz55 <bangladesh@amerdesh.com>
2021-09-02 14:51:22 +0600

    third commit

--- js/app.js
+++ js/app.js
@@ -1,26 +1,67 @@
+document.getElementById('error-message').style.display = 'none';
+document.getElementById('spinner').style.display = 'none';
+
 const searchInput = document.getElementById('search-input').addEventListener('click', function() {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
+    //clean search field
     searchField.value = '';
-    //load data
-    const url = `http://openlibrary.org/search.json?q=${searchText}`;
-    // console.log(url);
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displaySearchResult(data.docs));
+    document.getElementById('searchbook-result').textContent = '';
+
+    if (searchText === '') {
+        // please write something to display
+        displayError();
+    } else {
+        // Display Spinner
+        document.getElementById('spinner').style.display = 'block';
+        // Hide error
+        document.getElementById('error-message').style.display = 'none';
+        // Clear Search Result
+        document.getElementById('searchbook-result').textContent = '';
+        //clear prievious Book numbers
+        document.getElementById('book-numbers').textContent = '';
+
+        //load data
+        const url = `http://openlibrary.org/search.json?q=${searchText}`;
+        // console.log(url);
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displaySearchResult(data.docs));
+    }
+
 });
+
+const displayError = () => {
+    document.getElementById('error-message').style.display = 'block';
+    document.getElementById('spinner').style.display = 'none';
+    document.getElementById('book-numbers').textContent = '';
+
+}
 
 /* Display Search Result */
 const displaySearchResult = books => {
-    const searchResult = document.getElementById('searchBookResult');
-    /* use for each loop
-     Retrieve each book and display in a card */
-    books.forEach(book => {
-        document.getElementById('book-numbers').innerHTML = `Book Found ${books.length}`;
-        const divContainer = document.createElement('div');
-        divContainer.classList.add('col');
+    // clear team previous team number
+    // document.getlementById('book-numbers').textContent = '';
 
-        divContainer.innerHTML = `
+    //clear previous search result
+    const searchResult = document.getElementById('searchbook-result');
+    searchResult.textContent = '';
+    // console.log(books);
+    if (books.length === 0) {
+        displayError();
+    } else {
+        document.getElementById('error-message').style.display = 'none';
+        document.getElementById('spinner').style.display = 'none';
+
+        /* use for each loop
+         Retrieve each book and display in a card */
+        books.forEach(book => {
+            document.getElementById('book-numbers').textContent = `Book Found ${books.length}`;
+
+            const divContainer = document.createElement('div');
+            divContainer.classList.add('col');
+
+            divContainer.innerHTML = `
         <div class="card h-100">
             <img class="h-75" src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top h-75" alt="...">
             <div class="card-body">
@@ -31,6 +72,7 @@ const displaySearchResult = books => {
             </div>
         </div>
         `
-        searchResult.appendChild(divContainer);
-    });
-};
+            searchResult.appendChild(divContainer);
+        });
+    }
+}
