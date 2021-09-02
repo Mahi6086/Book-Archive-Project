51afe4750de7b8498e4e193c313d446763b782b0
Muaz55 <bangladesh@amerdesh.com>
2021-09-02 23:01:15 +0600

    submitted commit

--- js/app.js
+++ js/app.js
@@ -56,11 +56,12 @@ const displayError2 = () => {
 const displaySearchResult = books => {
     document.getElementById('book-numbers').textContent = `Book Found ${books.numFound}`;
 
+    const bookList = books.docs;
     //clear previous search result
     const searchResult = document.getElementById('searchbook-result');
     searchResult.textContent = '';
     // console.log(books);
-    if (books.docs.length === 0) {
+    if (bookList.length === 0) {
         displayError2();
     } else {
         document.getElementById('error-message2').style.display = 'none';
@@ -68,7 +69,7 @@ const displaySearchResult = books => {
 
         /* use for each loop
          Retrieve each book and display in a card */
-        books.docs.forEach(book => {
+        bookList.forEach(book => {
 
             const divContainer = document.createElement('div');
             divContainer.classList.add('rounded-3');
@@ -78,8 +79,8 @@ const displaySearchResult = books => {
             <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top h-75" alt="...">
             <div class="card-body">
                 <h3 class=""><span class="text-danger fw-bold">Books-Name:</span> ${book.title}</h3>
-                <h4 class=""><span class="text-danger fw-bold">Author:</span> ${book.author_name}</h4>
-                <h5 class=""><span class="text-danger fw-bold">First-Publish-Year:</span> ${book.first_publish_year}</h5>
+                <h4 class=""><span class="text-danger fw-bold">Author:</span> ${book.author_name ? book.author_name: 'N/A'}</h4>
+                <h5 class=""><span class="text-danger fw-bold">First-Publish-Year:</span> ${book.first_publish_year ? book.first_publish_year: 'N/A'}</h5>
                 <h5 class=""><span class="text-danger fw-bold">Publisher:</span> ${book.publisher[0]}</h5>
             </div>
         </div>
